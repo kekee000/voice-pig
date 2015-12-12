9979315fcc714fad4e791e25dcd18371ed2278ef
liuchaofan <liuchaofan@baidudeMacBook-Pro-6.local>
2015-12-12 12:59:51 +0800

    add demo files

---
+++ src/demo/main.js
@@ -0,0 +1,33 @@
+/**
+ * @author  刘超凡
+ * @file demo
+ */
+
+define(function (require) {
+    require("demo/parallax");
+    $('.pages').parallax({
+		direction: 'vertical', 	// horizontal (水平翻页)
+		swipeAnim: 'cover', 	// cover (切换效果)
+		drag:      true,		// 是否允许拖拽 (若 false 则只有在 touchend 之后才会翻页)
+		loading:   false,		// 有无加载页
+		indicator: false,		// 有无指示点
+		arrow:     false,		// 有无指示箭头
+		/*
+		 * 翻页后立即执行
+		 * {int} 		index: 第几页
+		 * {DOMElement} element: 当前页节点
+		 * {String}		directation: forward(前翻)、backward(后翻)、stay(保持原页)
+		 */
+		onchange: function(index, element, direction) {
+			// code here...
+			// console.log(index, element, direction);
+		},
+		/*
+		 * 横竖屏检测
+		 * {String}		orientation: landscape、protrait
+		 */
+		orientationchange: function(orientation) {
+			// console.log(orientation);
+		}
+	});
+});
